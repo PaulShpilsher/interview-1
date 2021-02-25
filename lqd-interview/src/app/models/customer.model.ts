98df1507db032ba58e1f011d32808e37e5399dd0
Paul Shpilsher <paul.shpilsher@gmail.com>
2021-02-25 20:58:55 +0300

    daa models

---
+++ lqd-interview/src/app/models/customer.model.ts
@@ -0,0 +1,13 @@
+
+export interface Customer {
+    id: string
+    firstName: string
+    lastName: string
+    emailAddress: string
+}
+
+
+export interface CustomerSearchRequest {
+    firstName: string
+    lastName: string
+}
